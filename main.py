29d887523cff7e50367620e677a3bb96f9e869a4
Abdurahman Ahmed <abdahmed@live.ca>
2019-07-31 16:15:19 -0600

    delete path added

--- main.py
+++ main.py
@@ -38,6 +38,16 @@ def index():
         return render_template('index.html', this_dict=this_dict, items=items, result=order,total=total)
     return render_template('index.html',this_dict=this_dict,items=items)
 
+@app.route('/delete/<value>',methods=['GET'])
+def delete_item(value):
+    global count
+    global total
+    value = int(value)
+    total = total - int(order[value]['total'])
+    count -= 1
+    del order[value]
+    return render_template('index.html', this_dict=this_dict, items=items, result=order,total=total)
+
 if __name__ == '__main__':
     app.debug=True
     server = Server(app.wsgi_app)
