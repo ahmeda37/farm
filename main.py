6c505e06a533d56a31425c4917529b86e00e52b1
Abdurahman Ahmed <abdahmed@live.ca>
2019-07-31 15:05:28 -0600

    show items, add to order

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 from livereload import Server
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import datetime
 app = Flask(__name__)
+this_dict ={
+    "customer": "First Last",
+    "address": "1234 Something Ave, Edmonton",
+    "sale_order": "1234",
+    "date": datetime.datetime.now()
+}
+items = {
+    "1":"Bananas",
+    "2":"Limes",
+    "3":"Mangos"
+}
+order={}
+count=0
+total=0
 
-
-@app.route('/')
+@app.route('/',methods =['POST','GET'])
 def index():
-    this_dict ={
-        "customer": "First Last",
-        "address": "1234 Something Ave, Edmonton",
-        "sale_order": "1234",
-        "date": datetime.datetime.now()
-    }
-    return render_template('index.html', this_dict=this_dict)
+    global count
+    global total
+    if request.method == 'POST':
+        result = request.form
+        item = {
+            'id':result['item-name'][:1],
+            'name':result['item-name'][2:],
+            'price':result['price'],
+            'quantity':result['quantity'],
+            'total':int(result['price']) * int(result['quantity'])
+        }
+        print(item)
+        order.update({count:item})
+        print(order)
+        count +=1
+        total += item['total']
+        return render_template('index.html', this_dict=this_dict, items=items, result=order,total=total)
+    return render_template('index.html',this_dict=this_dict,items=items)
 
 if __name__ == '__main__':
     app.debug=True
