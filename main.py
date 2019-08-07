6bc948f8ab1abd06aadcf39853b9d462814a19f1
Abdurahman Ahmed <abdahmed@live.ca>
2019-08-07 16:32:03 -0600

    add user auth --partial

--- main.py
+++ main.py
@@ -2,23 +2,44 @@ from livereload import Server
 from flask import Flask, render_template,request, redirect
 from flask_mysqldb import MySQL
 from config import user,password,db,host
+from werkzeug.security import generate_password_hash
+from flask_login import LoginManager,current_user,login_user,logout_user
 import datetime
 import queries
+
 app = Flask(__name__)
 app.config['MYSQL_USER'] = user
 app.config['MYSQL_PASSWORD'] = password
 app.config['MYSQL_DB'] = db
 app.config['MYSQL_HOST'] = host
 mysql = MySQL(app)
+login = LoginManager(app)
+
+from models import user
 
 open_order = False
 curCID = 0
 curOID = 0
 total=0
 
+@app.route('/signup',methods=['POST','GET'])
+def sign_up():
+    if current_user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        result = request.form
+        if user.get_user(result['username']) is None:
+            newUser = user.User()
+            newUser.User(username=result['username'],password=result['password'])
+            user.add_user(newUser)
+    return render_template('signup.html')
+
 @app.route('/',methods =['POST','GET'])
 def index():
     global total
+    #user = User.User()
+    #user.set_password('secret')
+    #print(user.check_password('seet'))
     if request.method == 'POST':
         result = request.form
         item = {
@@ -28,22 +49,22 @@ def index():
             'quantity':result['quantity'],
             'total':total+int(result['price']) * int(result['quantity'])
         }
-        myresult = queries.saveItem(item,curOID,mysql)
-        queries.updateTotal(item['total'],curOID,mysql)
+        myresult = queries.saveItem(item,curOID)
+        queries.updateTotal(item['total'],curOID)
         total = item['total']
     if open_order == True:
-        queries.updateTotal(total,curOID,mysql)
-        return render_template('index.html',customer=queries.getCustomer(curCID,mysql),items=queries.getProducts(mysql),result=queries.getOrderItems(curOID,mysql),total=total,curOID=curOID)
-    return render_template('index.html',customers=queries.getCustomers(mysql),items=queries.getProducts(mysql), result=queries.getOrderItems(curOID,mysql),total=total)
+        queries.updateTotal(total,curOID)
+        return render_template('index.html',customer=queries.getCustomer(curCID),items=queries.getProducts(),result=queries.getOrderItems(curOID),total=total,curOID=curOID)
+    return render_template('index.html',customers=queries.getCustomers(),items=queries.getProducts(), result=queries.getOrderItems(curOID),total=total)
 
 @app.route('/delete/<value>',methods=['GET'])
 def delete_item(value):
     global total
     value = int(value)
-    price = queries.getItem(str(value),mysql)
+    price = queries.getItem(str(value))
     total = total - (price[0]*price[1])
-    queries.deleteItem(str(value),mysql)
-    queries.updateTotal(total,curOID,mysql)
+    queries.deleteItem(str(value))
+    queries.updateTotal(total,curOID)
     return redirect("/")
 
 @app.route('/orders',methods=['GET'])
@@ -52,7 +73,7 @@ def showOrders():
     global total
     open_order = False
     total = 0
-    orders = queries.getOrders(mysql)
+    orders = queries.getOrders()
     ttotal = 0
     for key in orders:
         ttotal = ttotal + key[2]
@@ -67,13 +88,13 @@ def update_Order(id):
     print(type(id))
     open_order = True
     curOID = str(id)
-    curCID = str(queries.getCustomerBySO(id,mysql)[0])
-    total = int(queries.getTotal(curOID,mysql)[0])
-    return render_template('index.html',customer=queries.getCustomer(curCID,mysql),items=queries.getProducts(mysql),result=queries.getOrderItems(curOID,mysql),total=total,curOID=curOID)
+    curCID = str(queries.getCustomerBySO(id)[0])
+    total = int(queries.getTotal(curOID)[0])
+    return render_template('index.html',customer=queries.getCustomer(curCID),items=queries.getProducts(),result=queries.getOrderItems(curOID),total=total,curOID=curOID)
 
 @app.route('/orders/delete/<id>',methods=['GET'])
 def delete_Order(id):
-    queries.deleteOrder(id,mysql)
+    queries.deleteOrder(id)
     return redirect('/orders')
 
 @app.route('/invoices',methods=['GET','POST'])
@@ -82,12 +103,12 @@ def showInvoices():
     global total
     if request.method == 'POST':
         result = request.form
-        myresult = queries.paidOrder(mysql,curOID,curCID)
+        myresult = queries.paidOrder(curOID,curCID)
         return redirect('/invoices')
     open_order = False
     total = 0
     ttotal = 0
-    myresult = queries.getInvoices(mysql)
+    myresult = queries.getInvoices()
     print(myresult)
     for key in myresult:
         ttotal = ttotal + key[2] 
@@ -98,8 +119,8 @@ def setCustomer(id):
     global curCID
     global curOID
     if (open_order != True):
-        curCID = str(queries.getCustomer(id,mysql)[0])
-        curOID = str(queries.setOrder(id,mysql)[0])
+        curCID = str(queries.getCustomer(id)[0])
+        curOID = str(queries.setOrder(id)[0])
         open_order = True
     return redirect('/')
 
@@ -111,7 +132,7 @@ def add():
 def addCustomer():
     if request.method == 'POST':
         result = request.form
-        myresult = queries.addCustomer(mysql,result['name'],result['address'])
+        myresult = queries.addCustomer(result['name'],result['address'])
     return redirect('/add/customer/success')
 @app.route('/add/customer/success', methods=['GET'])
 def addCustomerSuccess():
@@ -121,11 +142,29 @@ def addCustomerSuccess():
 def addProduct():
     if request.method == 'POST':
         result = request.form
-        myresult = queries.addProduct(mysql,result['name'])
+        myresult = queries.addProduct(result['name'])
     return redirect('/add/product/success')
 @app.route('/add/product/success',methods=['GET'])
 def addProductSuccess(): 
     return render_template('add.html',alert='Product has been added')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        print('signed in')
+    #if request.method == 'POST':
+    curUser = user.get_user('ahmed')
+    if curUser is None or not user.check_password('secret'):
+        print('Invalid username or password')
+    else:
+        login_user(user)
+    print(curUser)
+    return redirect('/')
+
+@app.route('/logout',methods=['GET'])
+def logout():
+    logout_user()
+    return redirect('/')
 
 if __name__ == '__main__':
     app.debug=True
--- queries.py
+++ queries.py
@@ -1,26 +1,28 @@
+from main import mysql
+
 #Customers--------------------------------------
-def getCustomer(id,mysql):
+def getCustomer(id):
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM customers where cid="+id)
     mysql.connection.commit()
     myresult = cur.fetchone()
     cur.close()
     return myresult
-def getCustomerBySO(id,mysql):
+def getCustomerBySO(id):
     cur = mysql.connection.cursor()
     cur.execute("SELECT cid FROM sale_orders where oid="+str(id)+"")
     mysql.connection.commit()
     myresult = cur.fetchone()
     cur.close()
     return myresult
-def getCustomers(mysql):
+def getCustomers():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM customers")
     mysql.connection.commit()
     myresult = cur.fetchall()
     cur.close()
     return myresult
-def addCustomer(mysql,name,address):
+def addCustomer(name,address):
     cur = mysql.connection.cursor()
     cur.execute("INSERT into customers (name,address) VALUES ('"+name+"','"+address+"')")
     mysql.connection.commit()
@@ -28,14 +30,14 @@ def addCustomer(mysql,name,address):
     cur.close()
     return myresult
 #Products----------------------------------------
-def getProducts(mysql):
+def getProducts():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM products")
     mysql.connection.commit()
     myresult = cur.fetchall()
     cur.close()
     return myresult
-def addProduct(mysql,name):
+def addProduct(name):
     cur = mysql.connection.cursor()
     cur.execute("INSERT into products (name) VALUES ('"+name+"')")
     mysql.connection.commit()
@@ -43,7 +45,7 @@ def addProduct(mysql,name):
     cur.close()
     return myresult
 #Sale_Orders-------------------------------------
-def setOrder(id,mysql):
+def setOrder(id):
     cur = mysql.connection.cursor()
     cur.execute("INSERT INTO sale_orders (cid) VALUES("+id+");")
     mysql.connection.commit()
@@ -52,32 +54,32 @@ def setOrder(id,mysql):
     myresult = cur.fetchone()
     cur.close()
     return myresult
-def getOrders(mysql):
+def getOrders():
     cur = mysql.connection.cursor()
     cur.execute("select sale_orders.oid, customers.name, sale_orders.total, sale_orders.reg_date from sale_orders inner join customers on sale_orders.cid = customers.cid where sale_orders.paid = 0")
     mysql.connection.commit()
     myresult = cur.fetchall()
     return myresult
-def deleteOrder(value,mysql):
+def deleteOrder(value):
     cur = mysql.connection.cursor()
     cur.execute("DELETE from single_order where oid ="+str(value)+"")
     mysql.connection.commit()
     cur.execute("DELETE FROM sale_orders where oid ="+str(value)+"")
     mysql.connection.commit()
-def getTotal(id,mysql):
+def getTotal(id):
     cur = mysql.connection.cursor()
     cur.execute("SELECT total from sale_orders where oid ="+str(id)+"")
     mysql.connection.commit()
     myresult = cur.fetchone()
     return myresult
-def updateTotal(value,id,mysql):
+def updateTotal(value,id):
     cur = mysql.connection.cursor()
     cur.execute("UPDATE sale_orders SET total="+str(value)+" where oid ="+id+"")
     mysql.connection.commit()
     myresult = cur.fetchone()
     return myresult
 #Single_Order------------------------------------
-def saveItem(item,id,mysql):
+def saveItem(item,id):
     cur = mysql.connection.cursor()
     cur.execute("INSERT INTO single_order (oid,pid,price,quantity) VALUES ("+str(id)+","+str(item['id'])+","+str(item['price'])+","+str(item['quantity'])+")")
     mysql.connection.commit()
@@ -86,14 +88,14 @@ def saveItem(item,id,mysql):
     myresult = cur.fetchone()
     cur.close()
     return myresult
-def getItem(value,mysql):
+def getItem(value):
     cur = mysql.connection.cursor()
     cur.execute("SELECT price,quantity from single_order where singleid="+value+"")
     mysql.connection.commit()
     myresult = cur.fetchone()
     cur.close()
     return myresult
-def deleteItem(value,mysql):
+def deleteItem(value):
     cur = mysql.connection.cursor()
     cur.execute("DELETE FROM single_order where singleid ="+value+"")
     mysql.connection.commit()
@@ -101,7 +103,7 @@ def deleteItem(value,mysql):
     cur.close()
     return myresult
 #Sale_Orders Join Single_Order--------------------
-def getOrderItems(id,mysql):
+def getOrderItems(id):
     cur = mysql.connection.cursor()
     cur.execute("SELECT products.pid, products.name, single_order.singleid, single_order.price, single_order.quantity from single_order inner join products on products.pid = single_order.pid where single_order.oid = "+str(id)+"")
     mysql.connection.commit()
@@ -109,7 +111,7 @@ def getOrderItems(id,mysql):
     cur.close()
     return myresult
 
-def paidOrder(mysql,oid,cid):
+def paidOrder(oid,cid):
     cur = mysql.connection.cursor()
     cur.execute("INSERT into sale_invoices (oid,cid) values ("+oid+","+cid+")")
     mysql.connection.commit()
@@ -117,7 +119,7 @@ def paidOrder(mysql,oid,cid):
     mysql.connection.commit()
     cur.close()
 
-def getInvoices(mysql):
+def getInvoices():
     cur = mysql.connection.cursor()
     cur.execute("select sale_invoices.inid, customers.name, sale_orders.total, sale_invoices.reg_date from sale_invoices inner join customers on sale_invoices.cid = customers.cid inner join sale_orders on sale_invoices.oid = sale_orders.oid")
     mysql.connection.commit()
